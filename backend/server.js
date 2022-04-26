ab33bccd5057ff0576791f4c055ab26331b5a2fa
ISMAILBOUADDI <ismailbouaddi55@gmail.com>
2022-04-26 14:17:30 +0000

    create our server✔️💯

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,11 @@ mongooes.connect(URL, {
     }
 });
 
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+}
+);
+
 app.listen(process.env.PORT || 5000, () => {
     console.log('Server started');
 }
