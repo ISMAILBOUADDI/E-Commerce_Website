a71c1316097212601001a96937b20d870c9dbb82
ISMAILBOUADDI <ismailbouaddi55@gmail.com>
2022-05-05 01:22:21 +0000

    edit

--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -2,5 +2,5 @@ const router = require('express').Router();
 const userCtrl = require('../controller/userCtrl');
 
 router.post('/signup',userCtrl.signup);
-router.post('/refreshtoken',userCtrl.refreshToken);
+router.get('/refreshtoken',userCtrl.refreshToken);
 module.exports = router;
