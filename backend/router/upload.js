e2a448bd947f2bf25e567f26dbc972d8e4881ece
ISMAILBOUADDI <ismailbouaddi55@gmail.com>
2022-06-11 10:32:39 +0100

    upload

--- backend/router/upload.js
+++ backend/router/upload.js
@@ -2,15 +2,12 @@ const router = require('express').Router()
 const cloudinary = require('cloudinary')
 const auth = require('../middleware/auth')
 const authAdmin = require('../middleware/authAdmin')
-const uploadCtrl = require('../controllers/uploadCtrl')
+const uploadCtrl = require('../controller/uploadCtrl')
 
 // Upload image only admin can use
-router.post('/upload',auth , authAdmin, uploadCtrl.uploadImages)
+router.post('/upload',auth,authAdmin,uploadCtrl.uploadeImages)
 
 // Delete image only admin can use
-router.post('/destroy',auth , authAdmin, uploadCtrl.deleteImages)
-
-
-
+router.post('/destroy',auth,authAdmin,uploadCtrl.deleteImages)
 
 module.exports = router
