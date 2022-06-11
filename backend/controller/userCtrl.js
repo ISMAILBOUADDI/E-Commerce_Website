e78764222209ede6afa16c32dbcdf8d73bb095f0
ISMAILBOUADDI <ismailbouaddi55@gmail.com>
2022-06-11 10:28:30 +0100

    user ctrl

--- backend/controller/userCtrl.js
+++ backend/controller/userCtrl.js
@@ -1,4 +1,4 @@
-const User = require('../models/userModels');
+const User = require('../models/userModel');
 const bycryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
@@ -77,6 +77,39 @@ const userCtrl = {
       } catch (error) {
           
       }
+    },
+    getUser: async (req, res) =>{
+        try {
+            const user = await Users.findById(req.user.id).select('-password')
+            if(!user) return res.status(400).json({msg: "User does not exist."})
+
+            res.json(user)
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    addCart: async (req, res) =>{
+        try {
+            const user = await Users.findById(req.user.id)
+            if(!user) return res.status(400).json({msg: "User does not exist."})
+
+            await Users.findOneAndUpdate({_id: req.user.id}, {
+                cart: req.body.cart
+            })
+
+            return res.json({msg: "Added to cart"})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    history: async(req, res) =>{
+        try {
+            const history = await Payments.find({user_id: req.user.id})
+
+            res.json(history)
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
     }
 };
 const createAccessToken=(user)=>{
