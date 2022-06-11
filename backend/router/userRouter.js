6043d8c78876998100bb6b67b781123900eb059c
ISMAILBOUADDI <ismailbouaddi55@gmail.com>
2022-06-11 10:30:50 +0100

    user router

--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -1,12 +1,12 @@
 const router = require('express').Router()
-const userCtrl = require('../controllers/userCtrl')
+const userCtrl = require('../controller/userCtrl')
 const auth = require('../middleware/auth')
 
-router.post('/register', userCtrl.register)
+router.post('/register', userCtrl.signup)
 
 router.post('/login', userCtrl.login)
 
-router.get('/logout', userCtrl.logout)
+// router.get('/logout', userCtrl.logout)
 
 router.get('/refresh_token', userCtrl.refreshToken)
 
